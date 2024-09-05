565a2f72664f58491d2a86fa1a6ffff1d7042920
AdityaDendukuri <dendukuri.aditya123@outlook.com>
2024-09-05 16:25:15 +0530

    added lotka volterra notebook

--- examples/lotka_volterra.jl
+++ examples/lotka_volterra.jl
@@ -1,8 +1,10 @@
 
+
 using Catalyst: spatial_convert_err
 include("../src/DiscreteStochasticSimulation.jl")
 using .DiscreteStochasticSimulation
 using Catalyst
+using ProgressMeter
 using Plots
 using Expokit
 
@@ -20,35 +22,45 @@ experiment = begin
   model = DiscreteStochasticSystem(rn)
 
   # define boundaries (rectangular lattice in this case)
-  bounds = (0, 100)
+  bounds = (0, 500)
   boundary_condition(x) = RectLatticeBoundaryCondition(x, bounds)
 
   # define initial value and expand the space a bit 
-
-  U₀ = CartesianIndex(30, 30)
+  U₀ = CartesianIndex(100, 100)
   X = Set([U₀])
-
-  # expand
-  X = Expand!(X, model, boundary_condition, 20)
-  A = MasterEquation(X, model, [1.0, 0.25, 0.001], boundary_condition, 0.25)
-  PrintMatSorted(A, X)
-
-  # probability
-  P1 = zeros(bounds[2], bounds[2])
+  X = Expand!(X, model, boundary_condition, 2)
 
   # full initial probability vector
   p₀ = zeros(X |> length)
   p₀[DiscreteStochasticSimulation.FindElement(U₀, X)] = 1
 
+  # probability
+  P1 = zeros(bounds[2], bounds[2])
+
   δt = 0.01
+  global iter = 0
   global pₜ = p₀
-  a = @animate for t ∈ 0:0.01:3
+  a = @animate for t ∈ 0:0.01:10
+    #expand space and assemble matrix
+    X, pₜ = Expand!(X, pₜ, model, boundary_condition, 7)
+    A = MasterEquation(X, model, [1.0, 0.005, 0.6], boundary_condition, t)
+
+    # solve system and normalize 
     global pₜ = expmv(δt, A, pₜ)
     pₜ /= sum(pₜ)
-    P1 = zeros(bounds[2], bounds[2])
+
+    # purge lowest 80% probability states
+    X, pₜ = Purge!(X, pₜ, 80)
+
+    # set probability values to full space for visualization 
+    P1 .= 0
     P1[X|>collect] = pₜ
     f = heatmap(P1)
-  end
+    global iter = iter + 1
+    if iter % 50 == 0
+      println("t: $t, size: $(X |> collect |> length)")
+    end
+  end every 5
   gif(a, "anim_fps15.gif")
 
 end
